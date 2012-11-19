7598c20d8a8e8f2745defec3c269f6032d39b7fe
sofika <fischer@sofika.de>
2012-11-19 15:35:09 +0100

    initial commit

--- testbsp1/src/de/test/Test1.java
+++ testbsp1/src/de/test/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
 	{
 		System.out.println("Hallo KLAUS");
 		System.out.println("Hallo Das iste ien Aenderung");
+		System.out.println("Und noch eine Aenderung");
+		System.out.println("Und noch eine Aenderung");
 		
 	}
 }
