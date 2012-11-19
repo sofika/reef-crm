db8bdfae1023253fa11e7dcc87d6e089609d01ac
sofika <fischer@sofika.de>
2012-11-19 15:29:45 +0100

    Das ist ein Commit

---
+++ testbsp1/src/de/test/Test1.java
@@ -0,0 +1,10 @@
+package de.test;
+
+public class Test1 {
+	public static void main(String[] args)
+	{
+		System.out.println("Hallo KLAUS");
+		System.out.println("Hallo Das iste ien Aenderung");
+		
+	}
+}
